52e8ae5cbb46caebf05ebf0a6ac997234e4f48fc
Victor <santosfranciscosouza310@gmail.com>
2023-03-03 10:32:43 -0300

    Initial tests and first model controller view: Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department() { Id = 1, Name = "Eletronics" });
+            list.Add(new Department() { Id = 2, Name = "Marketing" });
+            return View(list);
+        }
+    }
+}
--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Projeto Sales Web MVC em netCore 2.1.";
+            ViewData["DevName"] = "Victor Francisco Santos de Souza";
 
             return View();
         }
